d2b4507940b1813b24f284cc50dfc3a10620a2ea
wings110 <283982871@qq.com>
2024-11-29 23:54:26 +0800

    first commit

---
+++ vendorsetup.sh
@@ -0,0 +1,34 @@
+#	GNU General Public License for more details.
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	OrangeFox is distributed in the hope that it will be useful,
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	See <http://www.gnu.org/licenses/>.
+#	This file is part of the OrangeFox Recovery Project
+#	any later version.
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+# 	Copyright (C) 2021-2024 The OrangeFox Recovery Project
+# 	Please maintain this if you use this script or any part of it
+# 	This software is released under GPL version 3 or any later version.
+
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_ASH_IS_BASH=1
+export FOX_DELETE_AROMAFM="1"
+export FOX_ENABLE_APP_MANAGER=1
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v27.0.zip
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export LC_ALL="C"
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+export OF_MAINTAINER="Azkali"
+export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_SCREEN_H="2700"
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+export OF_USE_MAGISKBOOT="1"
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+export TW_DEFAULT_LANGUAGE="en"
